3e2848ab94e61a76e6566271a521fe4d0f61a6d1
erikbrito <erikbrito01@gmail.com>
2020-10-08 01:46:11 -0300

    movieflix update part 2

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,32 +1,28 @@
 import React, { useEffect, useState } from 'react'
-import { View, StyleSheet, Image, ScrollView, FlatList, ActivityIndicator } from 'react-native'
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native'
 import { Searchbar } from 'react-native-paper'
-// import { Feather as Icon } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native'
 import api from '../services/api'
 
+interface movies {
+  title: string | null;
+  poster_path: string;
+}
+
 const Home = () => {
   const [SearchQuery, setSearchQuery] = useState<string>('');
-  const [data, setData] = useState([]);
+  const [movies, setMovies] = useState<any>([]);
 
   useEffect(() => {
-      api.get('movie/343611?api_key=0087505ff8561639dc9744342748aeed').then((repos) => {
-        const allRepos = repos.data;
-        setData(allRepos)
-        console.log(allRepos)
-      });
+    api.get('/movie?api_key=0087505ff8561639dc9744342748aeed').then((repos) => {
+      const allRepos = repos.data.results
+      setMovies(allRepos)
+    });
+
   }, []);
-
-
-  // const navigation = useNavigation()
-
-  // function handleNavigateTo
 
   return (
     <View style={styles.container}>
-      {/* <TouchableOpacity>
-        <Icon name="arrow-left" size={25} color="#9932CC" />
-      </TouchableOpacity> */}
 
       <Searchbar
         placeholder="Search"
@@ -37,35 +33,21 @@ const Home = () => {
 
       <ScrollView>
         <View style={styles.containerImages}>
-          <View style={{ flexDirection: 'row', margin: 20, justifyContent: "space-between" }}>
-            <Image
-              style={styles.images}
-              source={{
-                uri: `https://image.tmdb.org/t/p/w500/${data.poster_path}`,
-              }}
-            />
-            <Image
-              style={styles.images}
-              source={{
-                uri: `https://image.tmdb.org/t/p/w500/${data.poster_path}`,
-              }}
-            />
+
+          <View style={{ flexDirection: 'column', margin: 10, justifyContent: "space-between" }}>
+            {Object.keys(movies).map((index: any) => {
+              return (
+                <View key={index} style={styles.containerMovie}>
+                  <Image source={{ uri: `https://image.tmdb.org/t/p/w500/${movies[index].poster_path}` }} style={styles.images} />
+                  <View style={{ marginRight: 260 }}>
+                    <Text style={styles.title}>{movies[index].title}</Text>
+                    <Text numberOfLines={6} ellipsizeMode="tail" style={styles.overview}>{movies[index].overview}</Text>
+                  </View>
+                </View>
+              )
+            })}
           </View>
 
-          <View style={{ flexDirection: 'row', margin: 20, justifyContent: "space-between" }}>
-            <Image
-              style={styles.images}
-              source={{
-                uri: `https://image.tmdb.org/t/p/w500/${data.poster_path}`,
-              }}
-            />
-            <Image
-              style={styles.images}
-              source={{
-                uri: `https://image.tmdb.org/t/p/w500/${data.poster_path}`,
-              }}
-            />
-          </View>
         </View>
       </ScrollView>
 
@@ -83,67 +65,26 @@ const styles = StyleSheet.create({
   },
   containerImages: {
     display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-around",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  containerMovie: {
+    margin: 5,
+    flex: 1,
+    flexDirection: 'row'
   },
   images: {
-    width: 180,
-    height: 300,
+    width: 120,
+    height: 180,
+    margin: 5,
     justifyContent: "space-between"
-  }
+  },
+  title: {
+    fontWeight: 'bold'
+  },
+  overview: {
+    flexWrap: 'wrap'
+  },
 })
 
 export default Home
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import { View, Image, StyleSheet } from 'react-native';
-
-// const styles = StyleSheet.create({
-//   container: {
-//     paddingTop: 50,
-//   },
-//   tinyLogo: {
-//     width: 50,
-//     height: 50,
-//   },
-//   logo: {
-//     width: 66,
-//     height: 58,
-//   },
-// });
-
-// const DisplayAnImage = () => {
-//   return (
-//     <View style={styles.container}>
-//       <Image
-//         style={styles.tinyLogo}
-//         source={{
-//           uri: 'https://upload.wikimedia.org/wikipedia/pt/2/20/2_Fast_2_Furious_2003.jpg',
-//         }}
-//       />
-//       <Image
-//         style={styles.logo}
-//         source={{
-//           uri:
-//             'https://upload.wikimedia.org/wikipedia/pt/2/20/2_Fast_2_Furious_2003.jpg',
-//         }}
-//       />
-//     </View>
-//   );
-// }
-
-// export default DisplayAnImage;
--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,10 +1,12 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://api.themoviedb.org/3/',
+  baseURL: 'https://api.themoviedb.org/3/discover',
   headers: {
     'content-type': 'application/json'
   }
 })
 
 export default api
+
+// https://api.themoviedb.org/3/movie/343611?api_key=0087505ff8561639dc9744342748aeed
