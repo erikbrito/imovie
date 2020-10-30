afa57460d97086565b09fae49b05346809ecf29c
José Érik <erikbrito01@gmail.com>
2020-10-30 17:07:21 -0300

    removing comment

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { View, StyleSheet, Image, ScrollView, FlatList, ActivityIndicator } from 'react-native'
 import { Searchbar } from 'react-native-paper'
-// import { Feather as Icon } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native'
 import api from '../services/api'
 
@@ -10,23 +9,15 @@ const Home = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-      api.get('movie/343611?api_key=0087505ff8561639dc9744342748aeed').then((repos) => {
+      api.get(`movie/343611?api_key=${ process.env.API_KEY }`).then((repos) => {
         const allRepos = repos.data;
         setData(allRepos)
         console.log(allRepos)
       });
   }, []);
 
-
-  // const navigation = useNavigation()
-
-  // function handleNavigateTo
-
   return (
     <View style={styles.container}>
-      {/* <TouchableOpacity>
-        <Icon name="arrow-left" size={25} color="#9932CC" />
-      </TouchableOpacity> */}
 
       <Searchbar
         placeholder="Search"
@@ -94,56 +85,3 @@ const styles = StyleSheet.create({
 })
 
 export default Home
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import { View, Image, StyleSheet } from 'react-native';
-
-// const styles = StyleSheet.create({
-//   container: {
-//     paddingTop: 50,
-//   },
-//   tinyLogo: {
-//     width: 50,
-//     height: 50,
-//   },
-//   logo: {
-//     width: 66,
-//     height: 58,
-//   },
-// });
-
-// const DisplayAnImage = () => {
-//   return (
-//     <View style={styles.container}>
-//       <Image
-//         style={styles.tinyLogo}
-//         source={{
-//           uri: 'https://upload.wikimedia.org/wikipedia/pt/2/20/2_Fast_2_Furious_2003.jpg',
-//         }}
-//       />
-//       <Image
-//         style={styles.logo}
-//         source={{
-//           uri:
-//             'https://upload.wikimedia.org/wikipedia/pt/2/20/2_Fast_2_Furious_2003.jpg',
-//         }}
-//       />
-//     </View>
-//   );
-// }
-
-// export default DisplayAnImage;
