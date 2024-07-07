21e65626739a3afcfc64ce5b519f7c2ffdecf761
José Érik <erikbrito01@gmail.com>
2024-07-07 00:49:21 -0300

    fixing import

--- src/Pages/__test__/fieldSearch.test.tsx
+++ src/Pages/__test__/fieldSearch.test.tsx
@@ -2,7 +2,7 @@ import { render, fireEvent } from '@testing-library/react-native'
 import { Provider } from 'react-redux'
 import configureStore from 'redux-mock-store'
 
-import FieldSearch from 'src/Pages/fieldSearch'
+import FieldSearch from '@pages/fieldSearch'
 
 jest.mock('@react-navigation/native', () => ({
   ...jest.requireActual('@react-navigation/native'),
